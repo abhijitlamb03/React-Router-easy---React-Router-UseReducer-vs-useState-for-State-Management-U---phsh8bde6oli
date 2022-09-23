83f5fa0a8922e6cd7c94e164692d82f3abcfc8a8
Abhijit Mohan Lamb <106537258+abhijitlamb03@users.noreply.github.com>
2022-09-23 08:54:31 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
 
-export const Index = () => {
-  return <div id="index-page">Index Page</div>;
-};
+
+
+ReactDOM.render(<App />, document.getElementById("root"));
